9efee990895498134f6f76ee40e7248594d25c49
Cameron Grant <cameron.grant2611@gmail.com>
2021-09-03 14:06:39 +0100

    Abstract array

--- src/main/java/algorithm/visualiser/gui/RenderCanvas.java
+++ src/main/java/algorithm/visualiser/gui/RenderCanvas.java
@@ -3,23 +3,23 @@ package algorithm.visualiser.gui;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.util.Random;
+
+import algorithm.visualiser.util.Array;
 
 public class RenderCanvas extends Canvas {
 
     private int width;
     private int height;
 
-    private int[] array;
+    private Array array;
 
     public RenderCanvas(int width, int height) {
         this.width = width;
         this.height = height;
 
-        array = new int[25];
+        array = new Array(25);
 
-        createArray();
-        shuffleArray();
+        array.shuffle();
 
         this.setBackground(Color.DARK_GRAY);
     }
@@ -32,31 +32,14 @@ public class RenderCanvas extends Canvas {
         return height;
     }
 
-    public void createArray() {
-        for (int i = 0; i < array.length; i++) {
-            array[i] = 50 + (i * 20);
-        }
-    }
-
-    public void shuffleArray() {
-        Random random = new Random();
-
-        for (int i = array.length - 1; i > 0; i--) {
-            int index = random.nextInt(i + 1);
-            int temp = array[index];
-            array[index] = array[i];
-            array[i] = temp;
-        }
-    }
-
     public void renderBar(Graphics g, int index, int height) {
         g.fillRect((index * 25) + 225 , 600 - height, 25, height);
     }
 
     public void renderBars(Graphics g) {
         g.setColor(Color.WHITE);
-        for (int i = 0; i < array.length; i++) {
-            renderBar(g, i, array[i]);
+        for (int i = 0; i < array.size(); i++) {
+            renderBar(g, i, array.get(i));
         }
     }
 
---
+++ src/main/java/algorithm/visualiser/util/Array.java
@@ -0,0 +1,36 @@
+package algorithm.visualiser.util;
+
+import java.util.Random;
+
+public class Array {
+
+    private int[] array;
+
+    public Array(int size) {
+        array = new int[size];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 50 + (i * 20);
+        }
+    }
+
+    public int size() {
+        return array.length;
+    }
+
+    public int get(int index) {
+        return array[index];
+    }
+
+    public void shuffle() {
+        Random random = new Random();
+
+        for (int i = array.length - 1; i > 0; i--) {
+            int index = random.nextInt(i + 1);
+            int temp = array[index];
+            array[index] = array[i];
+            array[i] = temp;
+        }
+    }
+
+}
